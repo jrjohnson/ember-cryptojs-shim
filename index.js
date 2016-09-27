85be13f31c473abea2c599e8708f3db692ef5091
Sean Starnes <seanstar12@gmail.com>
2016-09-27 00:36:10 -0500

    Added Hmac modules.

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ module.exports = {
     app.import(app.bowerDirectory + '/crypto-js/crypto-js.js');
     app.import('vendor/cryptojs.js', {
       exports: {
-        CryptoJS: ['default', 'md5', 'sha1', 'sha256', 'sha512', 'sha3', 'ripemd160']
+        CryptoJS: [
+          'default',
+          'md5',
+          'sha1',
+          'sha256',
+          'sha512',
+          'sha3',
+          'ripemd160',
+          'hmac-mda5',
+          'hmac-sha1',
+          'hmac-sha256',
+          'hmac-sha3',
+          'hmac-sha512',
+          'hmac-ripemd160'
+        ]
       }
     });
   }
--- vendor/cryptojs.js
+++ vendor/cryptojs.js
@@ -15,4 +15,12 @@
   generateModule('cryptojs/sha512', { 'default': CryptoJS.SHA512 });
   generateModule('cryptojs/sha3', { 'default': CryptoJS.SHA3 });
   generateModule('cryptojs/ripemd160', { 'default': CryptoJS.RIPEMD160 });
+
+  generateModule('cryptojs/hmac-md5', { 'default': CryptoJS.HmacMD5 });
+  generateModule('cryptojs/hmac-sha1', { 'default': CryptoJS.HmacSHA1 });
+  generateModule('cryptojs/hmac-sha256', { 'default': CryptoJS.HmacSHA256 });
+  generateModule('cryptojs/hmac-sha3', { 'default': CryptoJS.HmacSHA3 });
+  generateModule('cryptojs/hmac-sha512', { 'default': CryptoJS.HmacSHA512 });
+  generateModule('cryptojs/hmac-ripemd160', { 'default': CryptoJS.HmacRIPEMD160 });
+
 })();
